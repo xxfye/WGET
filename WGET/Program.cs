77469fe91b722c3f870cc6d81dd0e54e645621c1
xxfye <xxfyee@gmail.com>
2018-07-18 23:14:15 +0800

    Frist version

---
+++ WGET/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WGET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            Console.WriteLine("This is frist version. No futures implemented");
+        }
+    }
+}
